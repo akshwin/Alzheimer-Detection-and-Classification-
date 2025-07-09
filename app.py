4240f3a87ac236b34054eb70c2524a0566fcd5e8
akshwin <akshwint.2003@gmail.com>
2025-07-09 11:12:22 +0530

    updated app

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ if uploaded_file:
 
         col1, col2 = st.columns(2)
         with col1:
-            st.image(img_path, caption="🧾 Uploaded MRI", use_column_width=True)
+            st.image(img_path, caption="🧾 Uploaded MRI", use_container_width=True)
         with col2:
             st.image(heatmap_path, caption="🔥 Grad-CAM Heatmap", use_column_width=True)
 
